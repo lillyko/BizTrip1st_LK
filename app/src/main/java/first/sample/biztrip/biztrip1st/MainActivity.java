0bad1cf52f4cbf8133fc3c671b874756c4ab8413
lillyko <lillykc0510@gmail.com>
2018-06-24 12:07:17 +0900

    'button'

--- app/src/main/java/first/sample/biztrip/biztrip1st/MainActivity.java
+++ app/src/main/java/first/sample/biztrip/biztrip1st/MainActivity.java
@@ -2,6 +2,7 @@ package first.sample.biztrip.biztrip1st;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ButtonOnClick(android.view.View v){
+
+        Toast.makeText(this, "Hello!", Toast.LENGTH_LONG).show();
+
     }
 }
